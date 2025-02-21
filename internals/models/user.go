5dfc113a195d4de63b8c596f867009cea7664555
muhammadrizkisetyanto <muhammadrizkisetyanto217@gmail.com>
2025-02-21 20:29:55 +0700

    commit user table (2a)

--- internals/models/user.go
+++ internals/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID        uint       `gorm:"primaryKey" json:"id"`
 	Name      string     `gorm:"size:50;not null" json:"name"`
 	Email     string     `gorm:"size:255;unique;not null" json:"email"`
-	Password  string     `gorm:"not null" json:"-"`
+	Password  string     `gorm:"not null" json:"password"`
 	GoogleID  *string    `gorm:"size:255;unique" json:"google_id,omitempty"`
 	Role      string     `gorm:"type:varchar(20);not null;default:'user'" json:"role"`
 	CreatedAt time.Time  `gorm:"autoCreateTime" json:"created_at"`
